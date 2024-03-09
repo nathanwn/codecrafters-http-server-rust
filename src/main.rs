1d41472d891597276f0a5e0551c7f8fd9e13e40a
nathanwn <49882031+nathanwn@users.noreply.github.com>
2024-03-09 21:35:13 +1000

    stage 2: respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
+use std::io;
+use std::io::Write;
 use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn handle_connection(tcp_stream: &mut TcpStream) -> io::Result<()> {
+    tcp_stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())?;
+    tcp_stream.flush()?;
+    Ok(())
+}
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // println!("Logs from your program will appear here!");
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut tcp_stream) => {
                 println!("accepted new connection");
+                handle_connection(&mut tcp_stream).expect("Responded successfully");
+                println!("done");
             }
             Err(e) => {
                 println!("error: {}", e);
