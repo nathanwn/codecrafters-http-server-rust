b09e8b3c78dc2125ff2a2a8e81bfa7d6f429a6d4
nathanwn <49882031+nathanwn@users.noreply.github.com>
2024-03-10 01:04:28 +1000

    stage 7: get a file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
+use clap::Parser;
+use std::fs;
 use std::io;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Write;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::path::Path;
 use std::thread;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    directory: Option<String>,
+}
 
 #[allow(dead_code)]
 #[derive(Debug)]
@@ -47,11 +58,12 @@ fn respond(
     tcp_stream: &mut TcpStream,
     respond_code: usize,
     msg: &str,
+    content_type: &str,
     body: &str,
 ) -> io::Result<()> {
     let response = [
         format!("HTTP/1.1 {} {}", respond_code, msg),
-        String::from("Content-Type: text/plain"),
+        format!("Content-Type: {}", content_type),
         format!("Content-Length: {}", body.len()),
         String::new(),
         String::from(body),
@@ -62,7 +74,7 @@ fn respond(
     Ok(())
 }
 
-fn handle_connection(tcp_stream: &mut TcpStream) -> io::Result<()> {
+fn handle_connection(tcp_stream: &mut TcpStream, directory: Option<String>) -> io::Result<()> {
     let mut reader = BufReader::new(tcp_stream.try_clone()?);
     let mut lines: Vec<String> = Vec::new();
     loop {
@@ -85,30 +97,49 @@ fn handle_connection(tcp_stream: &mut TcpStream) -> io::Result<()> {
         }
     }
     let request = parse_request(&lines);
-    if let Some(payload) = request.path.strip_prefix("/echo/") {
-        respond(tcp_stream, 200, "OK", payload)?
+    if directory.is_some() && request.method == "GET" && request.path.starts_with("/files/") {
+        let filename = request.path.strip_prefix("/files/").unwrap();
+        let filepath = Path::new(directory.unwrap().as_str()).join(filename);
+        if filepath.exists() {
+            let content = fs::read_to_string(filepath).expect("File should be read.");
+            respond(
+                tcp_stream,
+                200,
+                "OK",
+                "application/octet-stream",
+                content.as_str(),
+            )?
+        } else {
+            respond(tcp_stream, 404, "Not Found", "text/plain", "")?
+        }
+    } else if request.path.starts_with("/echo/") {
+        let payload = request.path.strip_prefix("/echo/").unwrap();
+        respond(tcp_stream, 200, "OK", "text/plain", payload)?
     } else if request.path == "/" {
-        respond(tcp_stream, 200, "OK", "")?
+        respond(tcp_stream, 200, "OK", "text/plain", "")?
     } else if request.path == "/user-agent" {
         if let Some(user_agent) = request.user_agent {
-            respond(tcp_stream, 200, "OK", user_agent.as_str())?
+            respond(tcp_stream, 200, "OK", "text/plain", user_agent.as_str())?
         } else {
             panic!()
         }
     } else {
-        respond(tcp_stream, 404, "Not Found", "")?
+        respond(tcp_stream, 404, "Not Found", "text/plain", "")?
     }
     Ok(())
 }
 
 fn main() {
+    let args = Args::parse();
+
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(mut tcp_stream) => {
                 println!("accepted new connection");
+                let directory = args.directory.clone();
                 thread::spawn(move || {
-                    handle_connection(&mut tcp_stream).expect("Responded successfully");
+                    handle_connection(&mut tcp_stream, directory).expect("Responded successfully");
                 });
                 println!("done");
             }
