ad0665fdf7587667d643c7d62d67939835fd29f3
nathanwn <49882031+nathanwn@users.noreply.github.com>
2024-03-10 14:59:28 +1000

    clean up

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ use std::net::TcpStream;
 use std::path::Path;
 use std::thread;
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -18,123 +17,158 @@ struct Args {
     directory: Option<String>,
 }
 
-#[allow(dead_code)]
 #[derive(Debug)]
-struct RequestHeader {
+struct HttpRequest {
     method: String,
     path: String,
     user_agent: Option<String>,
-    content_length: Option<usize>,
+    body: Option<String>,
 }
 
-fn parse_request_header(lines: &Vec<String>) -> RequestHeader {
-    let first_line = lines[0].clone();
-    let first_line_tokens: Vec<_> = first_line.split(" ").collect();
-    let method = String::from(first_line_tokens[0]);
-    let path = String::from(first_line_tokens[1]);
+#[derive(Debug)]
+struct HttpResponse {
+    respond_code: usize,
+    msg: String,
+    content_type: String,
+    body: Option<String>,
+}
 
-    let mut user_agent: Option<String> = None;
-    let mut content_length: Option<usize> = None;
-
-    for i in 1..lines.len() {
-        let line = lines[i].clone();
-        if let Some(val) = line.strip_prefix("User-Agent: ") {
-            user_agent = Some(String::from(val));
-        }
-        if let Some(val) = line.strip_prefix("Content-Length: ") {
-            content_length = Some(val.parse::<usize>().unwrap());
+impl HttpResponse {
+    fn new_ok_response(respond_code: usize, content_type: &str, body: Option<String>) -> Self {
+        HttpResponse {
+            respond_code,
+            msg: String::from("OK"),
+            content_type: String::from(content_type),
+            body,
         }
     }
 
-    return RequestHeader {
-        method,
-        path,
-        user_agent,
-        content_length,
-    };
+    fn new_not_found_response() -> Self {
+        HttpResponse {
+            respond_code: 404,
+            msg: String::from("Not Found"),
+            content_type: String::from("text/plain"),
+            body: None,
+        }
+    }
+
+    fn to_string(self) -> String {
+        let real_body: String = self.body.unwrap_or(String::new());
+        [
+            format!("HTTP/1.1 {} {}", self.respond_code, self.msg),
+            format!("Content-Type: {}", self.content_type),
+            format!("Content-Length: {}", real_body.len()),
+            String::new(),
+            real_body,
+        ]
+        .join("\r\n")
+    }
 }
 
-fn respond(
-    tcp_stream: &mut TcpStream,
-    respond_code: usize,
-    msg: &str,
-    content_type: &str,
-    body: &str,
-) -> io::Result<()> {
-    let response = [
-        format!("HTTP/1.1 {} {}", respond_code, msg),
-        format!("Content-Type: {}", content_type),
-        format!("Content-Length: {}", body.len()),
-        String::new(),
-        String::from(body),
-    ]
-    .join("\r\n");
-    tcp_stream.write(response.as_bytes())?;
-    tcp_stream.flush()?;
-    Ok(())
+trait HttpStream {
+    fn read_request(&mut self) -> io::Result<HttpRequest>;
+    fn write_response(&mut self, response: HttpResponse) -> io::Result<()>;
+}
+
+impl HttpStream for TcpStream {
+    fn read_request(&mut self) -> io::Result<HttpRequest> {
+        let mut reader = BufReader::new(self.try_clone()?);
+        let mut header_lines: Vec<String> = Vec::new();
+        loop {
+            let mut chars: Vec<u8> = Vec::new();
+            let mut line: Option<String> = None;
+            match reader.read_until('\n' as u8, &mut chars) {
+                Ok(_) => {
+                    (0..2).for_each(|_| {
+                        chars.pop();
+                    });
+                    line = Some(String::from_utf8(chars).unwrap());
+                }
+                _ => {}
+            }
+            if let Some(line_text) = line {
+                if line_text.len() == 0 {
+                    break;
+                }
+                header_lines.push(line_text);
+            }
+        }
+        let first_line = header_lines[0].clone();
+        let first_line_tokens: Vec<_> = first_line.split(" ").collect();
+        let method = String::from(first_line_tokens[0]);
+        let path = String::from(first_line_tokens[1]);
+
+        let mut user_agent: Option<String> = None;
+        let mut content_length: Option<usize> = None;
+
+        for i in 1..header_lines.len() {
+            let line = header_lines[i].clone();
+            if let Some(val) = line.strip_prefix("User-Agent: ") {
+                user_agent = Some(String::from(val));
+            }
+            if let Some(val) = line.strip_prefix("Content-Length: ") {
+                content_length = Some(val.parse::<usize>().unwrap());
+            }
+        }
+
+        let mut body: Option<String> = None;
+
+        if let Some(body_len) = content_length {
+            let mut body_buf: Vec<u8> = vec![0u8; body_len];
+            reader.read_exact(&mut body_buf)?;
+            body = Some(String::from_utf8(body_buf).unwrap());
+        }
+
+        Ok(HttpRequest {
+            method,
+            path,
+            user_agent,
+            body,
+        })
+    }
+
+    fn write_response(&mut self, response: HttpResponse) -> io::Result<()> {
+        self.write(response.to_string().as_bytes())?;
+        self.flush()?;
+        Ok(())
+    }
+}
+
+fn process_request(request: HttpRequest, directory: Option<String>) -> HttpResponse {
+    if let Some(dir) = directory {
+        if request.method == "POST" && request.body.is_some() {
+            let filename = request.path.strip_prefix("/files/").unwrap();
+            let filepath = Path::new(dir.as_str()).join(filename);
+            fs::write(filepath, request.body.unwrap()).expect("File should be written successfully.");
+            HttpResponse::new_ok_response(201, "text/plain", None)
+        } else if request.method == "GET" && request.path.starts_with("/files/") {
+            let filename = request.path.strip_prefix("/files/").unwrap();
+            let filepath = Path::new(dir.as_str()).join(filename);
+            if filepath.exists() {
+                let body = fs::read_to_string(filepath).ok();
+                HttpResponse::new_ok_response(200, "application/octet-stream", body)
+            } else {
+                HttpResponse::new_not_found_response()
+            }
+        } else {
+            HttpResponse::new_not_found_response()
+        }
+    } else if request.path.starts_with("/echo/") {
+        let body = request.path.strip_prefix("/echo/").map(str::to_string);
+        HttpResponse::new_ok_response(200, "text/plain", body)
+    } else if request.path == "/user-agent" {
+        HttpResponse::new_ok_response(200, "text/plain", request.user_agent)
+    } else if request.path == "/" {
+        HttpResponse::new_ok_response(200, "text/plain", None)
+    } else {
+        HttpResponse::new_not_found_response()
+    }
 }
 
 fn handle_connection(tcp_stream: &mut TcpStream, directory: Option<String>) -> io::Result<()> {
-    let mut reader = BufReader::new(tcp_stream.try_clone()?);
-    let mut header_lines: Vec<String> = Vec::new();
-    loop {
-        let mut chars: Vec<u8> = Vec::new();
-        let mut line: Option<String> = None;
-        match reader.read_until('\n' as u8, &mut chars) {
-            Ok(_) => {
-                (0..2).for_each(|_| {
-                    chars.pop();
-                });
-                line = Some(String::from_utf8(chars).unwrap());
-            }
-            _ => {}
-        }
-        if let Some(line_text) = line {
-            if line_text.len() == 0 {
-                break;
-            }
-            header_lines.push(line_text);
-        }
-    }
-    let header = parse_request_header(&header_lines);
-    if header.method == "POST" && header.content_length.is_some() && directory.is_some() {
-        let filename = header.path.strip_prefix("/files/").unwrap();
-        let filepath = Path::new(directory.unwrap().as_str()).join(filename);
-        let mut buf: Vec<u8> = vec![0u8; header.content_length.unwrap()];
-        reader.read_exact(&mut buf)?;
-        let content = String::from_utf8(buf).unwrap();
-        fs::write(filepath, content).expect("File should be written successfully.");
-        respond(tcp_stream, 201, "OK", "text/plain", "")?
-    } else if directory.is_some() && header.method == "GET" && header.path.starts_with("/files/") {
-        let filename = header.path.strip_prefix("/files/").unwrap();
-        let filepath = Path::new(directory.unwrap().as_str()).join(filename);
-        if filepath.exists() {
-            let content = fs::read_to_string(filepath).expect("File should be read successfully.");
-            respond(
-                tcp_stream,
-                200,
-                "OK",
-                "application/octet-stream",
-                content.as_str(),
-            )?
-        } else {
-            respond(tcp_stream, 404, "Not Found", "text/plain", "")?
-        }
-    } else if header.path.starts_with("/echo/") {
-        let payload = header.path.strip_prefix("/echo/").unwrap();
-        respond(tcp_stream, 200, "OK", "text/plain", payload)?
-    } else if header.path == "/" {
-        respond(tcp_stream, 200, "OK", "text/plain", "")?
-    } else if header.path == "/user-agent" {
-        if let Some(user_agent) = header.user_agent {
-            respond(tcp_stream, 200, "OK", "text/plain", user_agent.as_str())?
-        } else {
-            panic!()
-        }
-    } else {
-        respond(tcp_stream, 404, "Not Found", "text/plain", "")?
-    }
-    Ok(())
+    let request = tcp_stream.read_request().unwrap();
+    let response = process_request(request, directory);
+    tcp_stream.write_response(response)
 }
 
 fn main() {
